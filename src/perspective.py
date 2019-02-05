49fd1d620b08aaef85e48658f1740b73ad9cf6bf
Lucas Coelho Figueiredo <lucas.figueiredo@hexagonmining.com>
2019-02-05 20:57:35 -0200

    Perspective, detect lane pixels, curvature determination and vehicle position done.

---
+++ src/perspective.py
@@ -0,0 +1,12 @@
+import config
+import numpy as np
+import cv2
+
+
+def transform(img):
+    img_size = (img.shape[1], img.shape[0])
+    src = np.float32(config.get("trapezoid"))
+    dest = np.float32(config.get("dest_points"))
+    m = cv2.getPerspectiveTransform(src, dest)
+    warped = cv2.warpPerspective(img, m, img_size, flags=cv2.INTER_NEAREST)
+    return warped
